a1f89abbcedcf6373e423fc3c607e9c77c8044a3
leojazz28 <leonardosousa.linux@gmail.com>
2024-12-04 20:46:03 -0300

    arquivos aulas aovivo

---
+++ conexao-html-js/script.js
@@ -0,0 +1,18 @@
+const logo = document.getElementById("logo");
+
+console.log(logo)
+//--------------------------------------------------------------
+
+const postAutor =  document.getElementsByClassName("post-autor");
+
+console.log(postAutor)
+//--------------------------------------------------------------
+
+const texto = document.querySelector("#post02 .post-texto strong")
+
+console.log(texto);
+//---------------------------------------------------------------
+
+const textoDosPost = document.querySelectorAll("strong")
+
+console.log(textoDosPost[0].innerText);
