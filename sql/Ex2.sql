7dbdca5ba89704f80ba6fca00c33893e27e80a34
leojazz <leonardosousa.linux@gmail.com>
2025-02-16 11:44:39 -0300

    Ex2 sql

--- sql/Ex2.sql
+++ sql/Ex2.sql
@@ -1,7 +1,20 @@
  
 CREATE TABLE Clientes (
-    ClienteID INT AUTO_INCREMENT PRIMARY KEY,
+    ClienteID SERIAL PRIMARY KEY,
     Nome VARCHAR(100) NOT NULL,
     Email VARCHAR(100) NOT NULL UNIQUE,
     Telefone VARCHAR(15)
 );
+
+CREATE TABLE Pedidos (
+    PedidoID SERIAL PRIMARY KEY,
+    ClienteID INT,
+    DataPedido DATE NOT NULL,
+    ValorTotal DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
+);
+
+SELECT Clientes.Nome, Pedidos.DataPedido, Pedidos.ValorTotal
+FROM Clientes
+INNER JOIN Pedidos ON Clientes.ClienteID = Pedidos.ClienteID;
+
